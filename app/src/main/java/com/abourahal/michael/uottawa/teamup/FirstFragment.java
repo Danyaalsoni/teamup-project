0028f93e292beafeec1468d303fd8e0800148991
Michael <mabourahal@hotmail.com>
2017-07-16 15:11:40 -0400

    Navigation Menu

---
+++ app/src/main/java/com/abourahal/michael/uottawa/teamup/FirstFragment.java
@@ -0,0 +1,23 @@
+package com.abourahal.michael.uottawa.teamup;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hocke on 2017-07-16.
+ */
+
+public class FirstFragment extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout,container,false);
+        return myView;
+    }
+}
