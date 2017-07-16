d36a26c61fb6439d40cc7fc4fd4c68c0b71b91da
Danyaal Soni <danyaalsoni@gmail.com>
2017-07-16 17:08:35 -0400

    Added profile activity

--- app/src/main/java/com/abourahal/michael/uottawa/teamup/NavigationActivity.java
+++ app/src/main/java/com/abourahal/michael/uottawa/teamup/NavigationActivity.java
@@ -1,5 +1,6 @@
 package com.abourahal.michael.uottawa.teamup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -41,6 +43,8 @@ public class NavigationActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
