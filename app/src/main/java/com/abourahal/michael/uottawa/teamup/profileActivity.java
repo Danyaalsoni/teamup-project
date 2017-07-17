fae465efc4324553df5c152f2e485c85e324683f
Danyaal Soni <danyaalsoni@gmail.com>
2017-07-17 19:58:47 -0400

    Completed profile activiy xml

--- app/src/main/java/com/abourahal/michael/uottawa/teamup/profileActivity.java
+++ app/src/main/java/com/abourahal/michael/uottawa/teamup/profileActivity.java
@@ -14,39 +14,13 @@ import java.io.IOException;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class profileActivity extends AppCompatActivity {
-    private int PICK_IMAGE_REQUEST=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        CircleImageView image=(CircleImageView) findViewById(R.id.profile_image);
-        image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/jpeg");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-            }
-        });
 
 
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-
-            Uri uri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                CircleImageView image=(CircleImageView)findViewById(R.id.profile_image);
-                image.setImageBitmap(bitmap);
-                System.out.println(uri.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
