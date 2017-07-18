565196d747c8c36884785bc508195b3615db71a4
Danyaal Soni <danyaalsoni@gmail.com>
2017-07-17 21:35:15 -0400

    completed ui for editprofile activity

--- app/src/main/java/com/abourahal/michael/uottawa/teamup/editProfileActivity.java
+++ app/src/main/java/com/abourahal/michael/uottawa/teamup/editProfileActivity.java
@@ -1,5 +1,6 @@
 package com.abourahal.michael.uottawa.teamup;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -7,13 +8,19 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import java.io.IOException;
+import java.util.Calendar;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class editProfileActivity extends AppCompatActivity {
     private int PICK_IMAGE_REQUEST=1;
+    private Button date;
+    private int year,month,day;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +35,28 @@ public class editProfileActivity extends AppCompatActivity {
                 startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
             }
         });
+        date=(Button)findViewById(R.id.dateButton);
+
+    }
+    public void launchDatepicker(View view){
+        final Calendar c=Calendar.getInstance();
+        year=c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        date.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
