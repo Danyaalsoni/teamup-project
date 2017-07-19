7c36dd26a5ef49165ec27e4eeda0d70e28f66b90
Daneyal <daneyalb254@gmail.com>
2017-07-18 22:38:46 -0400

    layout changes with icons

--- app/src/main/java/com/abourahal/michael/uottawa/teamup/NavigationActivity.java
+++ app/src/main/java/com/abourahal/michael/uottawa/teamup/NavigationActivity.java
@@ -1,11 +1,8 @@
 package com.abourahal.michael.uottawa.teamup;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.app.FragmentManager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -113,12 +111,8 @@ public class NavigationActivity extends AppCompatActivity
         } else if (id == R.id.nav_second_layout) {
             fragmentManager.beginTransaction().replace(R.id.content_frame,new SecondFragment()).commit();
 
-        } else if (id == R.id.nav_third_layout) {
-            fragmentManager.beginTransaction().replace(R.id.content_frame,new ThirdFragment()).commit();
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_help) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new ThirdFragment()).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
